cee14b6e0d59402273800f1898f6a9e3768da78f
alesha reed <aleshak83@gmail.com>
2019-10-28 19:53:11 -0500

    added farmers

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import 'bootstrap';
 import '../styles/main.scss';
 import cowList from './components/cowList/cowList';
+import farmerList from './components/farmerList/farmerList';
 
 const init = () => {
   cowList.buildCows();
   console.error('hi');
+  farmerList.buildFarmers();
 };
 
 init();
