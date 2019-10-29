c4cf662a74500d0ad4f7b3595e9a80a68d143955
alesha reed <aleshak83@gmail.com>
2019-10-28 19:14:37 -0500

    added promises

--- src/javascripts/components/cowList/cowList.js
+++ src/javascripts/components/cowList/cowList.js
@@ -4,13 +4,7 @@ import cowData from '../../helpers/data/cows';
 const buildCows = () => {
   cowData.getCows()
     .then((response) => {
-      const demCows = response.data.cows;
-      const cows = [];
-      Object.keys(demCows).forEach((cowId) => {
-        demCows[cowId].id = cowId;
-        cows.push(demCows[cowId]);
-      });
-      console.error('it worked', cows);
+      console.log('cow array from cowList', response);
     })
     .catch((error) => {
       console.error('broke', error);
